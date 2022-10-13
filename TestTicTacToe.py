56a7c9ea6483686323307c28600f37b95a0953c3
TeamBearCrew <pandakenfire@gmail.com>
2022-10-13 15:11:41 +0300

    Added another one test for TicTacToe

--- TestTicTacToe.py
+++ TestTicTacToe.py
@@ -8,5 +8,10 @@ class testTicTacToe(unittest.TestCase):
         test_case = "-------------\n| 1 | 2 | 3 |\n-------------\n| 4 | 5 | 6 |\n-------------\n| 7 | 8 | 9 |\n-------------"
         self.assertEqual(test_case, TicTacToe.dashboard(board=list(range(1,10))))
 
+    def test_two(self):
+        test_case = "-------------\n| 1 | 2 | 3 |\n-------------\n| 4 | 5 | 6 |\n-------------\n| 7 | 8 | 9 |\n-------------"
+        self.assertEqual(test_case, TicTacToe.dashboard(board=list(range(5, 8))))
+
+
 if __name__ == "__main__":
     unittest.main()
